d484174933d9dedfd335342b1c87190f773e0445
Olha Fastiuk <olhafastiuk@gmail.com>
2021-12-28 13:00:42 +0200

    Create contacts page

---
+++ src/Components/Arrays.js
@@ -0,0 +1,7 @@
+import f1 from "../images/gallery/1.jpeg";
+import f2 from "../images/gallery/2.jpeg";
+import f3 from "../images/gallery/3.jpeg";
+import f4 from "../images/gallery/4.jpeg";
+import f5 from "../images/gallery/5.jpeg";
+
+export let Photos = [f1, f2, f3, f4, f5, f1];
